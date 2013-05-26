cda2c09738eeb96e8f14d9b2644a37477ac9abcc
Sanower <loveman24@gmail.com>
2013-05-27 01:03:25 +0600

    Html CSS from Desktop

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Online Radio</title>
         <link rel="stylesheet" type="text/css" href="css/style.css" />
+	<script type="text/javascript" src="js/jquery-1.10.0.min.js"></script>
+	<script type="text/javascript" src="js/js.js"></script>
     </head>
     <body>
         <div id="page">
@@ -39,12 +41,72 @@
                             Schedule
                         </div>
                         <div id="weekly_schedule_title">
-                            <span id="weekly">Weekly</span>/ <span id="daily">Daily</span>
+                            <span id="daily">Daily</span> / <span id="weekly">Weekly</span>
                         </div>
                     </div>
                 </div>
                 <div id="schedule_box">
-                    
+		    <div id="schedule_innter">
+			<div id="daily_schedule">
+			    <ul>
+				<li><span class="time">09:30</span> - Talk show 1</li>
+				<li><span class="time">10:30</span> - Talk show 9</li>
+				<li><span class="time">11:30</span> - Talk show 3</li>
+				<li><span class="time">13:30</span> - Talk show 8</li>
+				<li><span class="time">15:30</span> - Talk show 4</li>
+				<li><span class="time">18:30</span> - Talk show 5</li>
+			    </ul>
+			</div>
+			<div id="weekly_schedule">
+			    <div class="week_days">Saturday</div>
+			    <ul>
+				<li><span class="time">09:30</span> - Talk show 1</li>
+				<li><span class="time">10:30</span> - Talk show 9</li>
+				<li><span class="time">11:30</span> - Talk show 3</li>
+				<li><span class="time">13:30</span> - Talk show 8</li>
+			    </ul>
+			    <div class="week_days">Sunday</div>
+			    <ul>
+				<li><span class="time">09:30</span> - Talk show 1</li>
+				<li><span class="time">10:30</span> - Talk show 9</li>
+				<li><span class="time">11:30</span> - Talk show 3</li>
+				<li><span class="time">13:30</span> - Talk show 8</li>
+			    </ul>
+			    <div class="week_days">Monday</div>
+			    <ul>
+				<li><span class="time">09:30</span> - Talk show 1</li>
+				<li><span class="time">10:30</span> - Talk show 9</li>
+				<li><span class="time">11:30</span> - Talk show 3</li>
+				<li><span class="time">13:30</span> - Talk show 8</li>
+			    </ul>
+			    <div class="week_days">Tuesday</div>
+			    <ul>
+				<li><span class="time">10:30</span> - Talk show 9</li>
+				<li><span class="time">11:30</span> - Talk show 3</li>
+				<li><span class="time">13:30</span> - Talk show 8</li>
+			    </ul>
+			    <div class="week_days">Wednesday</div>
+			    <ul>
+				<li><span class="time">09:30</span> - Talk show 1</li>
+				<li><span class="time">10:30</span> - Talk show 9</li>
+				<li><span class="time">11:30</span> - Talk show 3</li>
+				<li><span class="time">13:30</span> - Talk show 8</li>
+			    </ul>
+			    <div class="week_days">Thursday</div>
+			    <ul>
+				<li><span class="time">09:30</span> - Talk show 1</li>
+				<li><span class="time">10:30</span> - Talk show 9</li>
+				<li><span class="time">11:30</span> - Talk show 3</li>
+				<li><span class="time">13:30</span> - Talk show 8</li>
+			    </ul>
+			    <div class="week_days">Friday</div>
+			    <ul>
+				<li><span class="time">09:30</span> - Talk show 1</li>
+				<li><span class="time">10:30</span> - Talk show 9</li>
+				<li><span class="time">13:30</span> - Talk show 8</li>
+			    </ul>
+			</div>
+		    </div>
                 </div>
                 
                 <div class="col_title">
@@ -54,6 +116,16 @@
                         </div>
                     </div>
                 </div>
+		<div id="archieve_box">
+		    <div id="archieve_box_inner">
+			<ul>
+			    <li>Talk show 1</li>
+			    <li>Talk show 9</li>
+			    <li>Talk show 3</li>
+			    <li>Talk show 8</li>
+			</ul>
+		    </div>
+		</div>
             </div>
             <div class="cl"></div>
         </div>
--- js/js.js
+++ js/js.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
+    $("#weekly").click(function(){
+	$("#daily_schedule").fadeOut();
+	$("#weekly_schedule").fadeIn();
+    });
+    $("#daily").click(function(){
+	$("#weekly_schedule").fadeOut();
+	$("#daily_schedule").fadeIn();
+    });
     
 });
