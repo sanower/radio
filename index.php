cccd00a351cce649ce11111a988b8d62fe021594
sanower <loveman24@gmail.com>
2013-05-26 18:04:41 +0600

    First Version

--- index.php
+++ index.php
@@ -2,11 +2,61 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>Online Radio</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div id="page">
+            <div id="col1">
+                <div class="col_title">
+                    <img src="images/title1.jpg" title="Title" />
+                </div>
+                <div class="col_box">
+                    <div id="player">
+
+                    </div>
+                    <div class="ads">
+                        <img src="images/add.jpg" alt="Ads" />
+                    </div>
+                    <div class="ads">
+                        <img src="images/add.jpg" alt="Ads" />
+                    </div>
+                </div>
+            </div>
+            <div id="col2">
+                <div class="col_title">
+                    <img src="images/title_2.jpg" title="Title" />
+                </div>
+                <div class="col_content">
+                    <div id="side_title"><img src="images/side_title.jpg" /></div>
+                    <div id="cc_recenttracks_main" class="cc_recenttracks_list">Loading ...</div>
+                </div>
+            </div>
+            <div id="col3">
+                <div class="col_title">
+                    <div class="title_bg">
+                        <div id="schedule_title">
+                            Schedule
+                        </div>
+                        <div id="weekly_schedule_title">
+                            <span id="weekly">Weekly</span>/ <span id="daily">Daily</span>
+                        </div>
+                    </div>
+                </div>
+                <div id="schedule_box">
+                    
+                </div>
+                
+                <div class="col_title">
+                    <div class="title_bg">
+                        <div id="archieve_title">
+                            Archive (Recent podcasts)
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="cl"></div>
+        </div>
+        <script language="javascript" type="text/javascript" src="http://198.100.148.77:2199/system/recenttracks.js"></script>
     </body>
 </html>
